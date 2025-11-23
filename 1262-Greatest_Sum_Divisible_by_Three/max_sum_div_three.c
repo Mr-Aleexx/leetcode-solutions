9f03039acb5263f699408f0261c81eca8028e8df
Alex <alex.hazet@gmail.com>
2025-11-23 20:16:57 +0100

    Base Code

---
+++ 1262-Greatest_Sum_Divisible_by_Three/max_sum_div_three.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int maxSumDivThree(int* nums, int numsSize) {
+	int res;
+
+
+	return res;
+}
+
+
+int main () {
+	int nums[] = {3,6,5,1,8 };
+	int n      = 5;
+	
+
+	printf("Output : %d\n", maxSumDivThree(nums, n));
+}
