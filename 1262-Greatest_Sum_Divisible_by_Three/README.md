65191d6c17d484fbc5a9e2937794b70fa74028db
Alex <alex.hazet@gmail.com>
2025-11-23 21:14:58 +0100

    It's sunday bro, I played GTA and worked on a discord bot

---
+++ 1262-Greatest_Sum_Divisible_by_Three/README.md
@@ -0,0 +1,8 @@
+# 1262-Greatest_Sum_Divisible_by_Three
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSySDArKAOS56QOXF90BPhI1neLujwFfniXMQ&s" alt="icantproveit">
+I know it's a greedy algorithm but i can't prove it 
+
+More seriously I don't have time to do this one since it's sunday (might do it tomorrow)
+
+My first idea was to sort the array, then take the sum of it and take out nums[i], but it's a litlle bit more difficult.
--- 1262-Greatest_Sum_Divisible_by_Three/max_sum_div_three.c
+++ 1262-Greatest_Sum_Divisible_by_Three/max_sum_div_three.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int maxSumDivThree(int* nums, int numsSize) {
-	int res;
+int comp(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
 
-
+int int_array_sum(int* nums, int numsSize) {
+	int res = 0;
+	for (int i = 0 ; i < numsSize ; i++ ) {
+		res += nums[i];
+	}
 	return res;
 }
 
 
+int maxSumDivThree(int* nums, int numsSize) {
+	int res = 0;
+	qsort(nums, numsSize, sizeof(int),comp);
+	int numsSum, tempSum = int_array_sum(nums, numsSize);
+
+	for (int i = 0 ; i < numsSize ; i++) {
+		
+		if (tempSum % 3 == 0) {
+			return res;
+		}
+		tempSum -= nums[i];
+	}
+
+	return 0;
+}
+
+
 int main () {
-	int nums[] = {3,6,5,1,8 };
+	int nums[] = {3, 6, 5, 1, 8};
 	int n      = 5;
 	
 
