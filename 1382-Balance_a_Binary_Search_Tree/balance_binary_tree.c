81e536a7d751f2c78f122033319009d0b4c5bc79
Hazet Alex <alex.hazet@gmail.com>
2026-02-09 14:22:56 +0100

    don't have time

---
+++ 1382-Balance_a_Binary_Search_Tree/balance_binary_tree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* balanceBST(struct TreeNode* root) {
+    
+}
+
+int main () {
+        
+
+    return 0;
+}
