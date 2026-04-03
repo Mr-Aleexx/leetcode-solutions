39c0046de444a23d6662e5e4efc58a7103ae934f
Alex <alex.hazet@gmail.com>
2026-04-03 20:22:03 +0200

    i'm back finally,  integer to roman

--- 12-Integer_to_Roman/integer_to_roman.c
+++ 12-Integer_to_Roman/integer_to_roman.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char *intToRoman(int num) {
-	
+int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+char* roman_chars[] = {"M","CM","D","CD",
+                       "C","XC","L","XL",
+                       "X","IX","V","IV","I"};
+
+char* intToRoman(int num) {
+
+    char* res = malloc(100000000);  
+    res[0] = '\0';
+
+    int i = 0;
+
+    while(num > 0) {
+
+        if(num >= values[i]) {
+            strcat(res, roman_chars[i]);
+            num -= values[i];
+        } 
+        else {
+            i++;
+        }
+    }
+
+    return res;
 }
 
 int main() {
-	int input;
 
-	printf("Enter the number : %d", input);
-	scanf("%s", input);
+    int input;
+    printf("Enter the number : ");
+    scanf("%d", &input);
 
-	char* output = intToRoman(input);
-	return 0;
+    char* output = intToRoman(input);
+
+    printf("output : %s\n", output);
+
+    free(output);
 }
--- 128-Longest_Consecutive_Sequence/longest_sequence.c
+++ 128-Longest_Consecutive_Sequence/longest_sequence.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef i8 b8;
+typedef i32 b32;
 
 #define MAX(a, b) (a > b ? a : b)
+#define TABLE_SIZE 100000
 
-//TODO : implement hashsets since it's too slow
+typedef struct entry {
+    u32 key;
+    u32 value;
+    struct entry* next;
+} entry;
+
+typedef struct HashTable {
+    entry** entries;
+} HashTable;
+
+HashTable* create_ht() {
+    HashTable* ht = malloc(sizeof(HashTable) * 1);
+    ht->entries   = malloc(sizeof(entry*) * TABLE_SIZE);
+    
+    for (int i= 0 ; i < TABLE_SIZE ; i++) {
+        ht->entries[i] = NULL;
+    }
+    return ht;
+}
+
+u32 hash(const char* key) {
+    u64 value = 0;
+    u32 key_len = strlen(key);
+    
+    for (u32 i = 0 ; i < key_len ; ++i) {
+        value = value *  37 + key[i];
+    }
+
+    value %= TABLE_SIZE;
+
+    return value;
+}
+
 int in_array(int* array, int n, int number) {
     for (int i = 0 ; i < n ; i++) {
         if (array[i] == number) {
@@ -42,6 +90,11 @@ int main () {
 
     printf("Output : %d\n",longest_consecutive(array, n));
     printf("Output : %d\n",longest_consecutive(array2, n2));
+
+    printf("------- Hash Test -------\n");
+
+    const char* key = "i am the best of the world";
+    printf("hash result %d\n", hash(key) );
 
     return 0;
 }
